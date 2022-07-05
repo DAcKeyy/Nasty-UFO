6197a39377814afb7b9c9f3ef32966c439fadf40
DovmantovAK <alexanderdovmantov@gmail.com>
2022-07-05 13:48:12 +0300

    addition [GAME IS DISABLED]: Added ScriptableObject "LevelGenerationScriptableObjectSettings" and "UFO Game" scene

---
+++ Assets/Scripts/Data/Generators/LevelGenerationScriptableObjectSettings.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace Data.Generators
+{
+	[CreateAssetMenu(fileName = "Data", menuName = "ScriptableObjects/Level Generation Settings", order = 1)]
+	public class LevelGenerationScriptableObjectSettings : ScriptableObject
+	{
+		public NastyUFOLevelGenerationSettings _settings;
+	}
+}
